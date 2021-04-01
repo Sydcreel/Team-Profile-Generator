3dee03a2bab0660b8df1b7ba0354571253dbe0b3
Sydney Creel <sydney.creel@outlook.com>
2021-04-01 09:47:10 -0500

    updated

--- src/template.js
+++ src/template.js
@@ -16,14 +16,14 @@ module.exports = (teamMembers) => {
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Your Team</title>
+            <title>Team Roster</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
             <link rel="stylesheet" href='./style.css'>
         </head>
         <body>
             <header class=" has-background-danger has-text-centered p-6">
-                <h1 class="title is-2 has-text-white">MyTeam</h1>
+                <h1 class="title is-2 has-text-white">Team Roster</h1>
             </header>
             <main class="is-flex is-justify-content-center">
                 <div class="column is-four-fifths is-full-mobile is-flex is-justify-content-center is-flex-wrap-wrap">
@@ -33,7 +33,7 @@ module.exports = (teamMembers) => {
         </body>
         </html>`;
     if (teamHtml) {
-      console.log("Generating Roster...");
+      console.log("Generating Team Roster...");
       resolve(teamHtml);
     } else {
       reject();
